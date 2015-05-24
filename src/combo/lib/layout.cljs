5e9aad808e24472a8607d18fee3f6f337f08989a
Ilshad Khabibullin <astoon.net@gmail.com>
2015-05-24 13:20:15 +0300

    bootstrap layout: do not pass widget-level layout if it is defined in spec

--- src/combo/lib/layout.cljs
+++ src/combo/lib/layout.cljs
@@ -5,8 +5,7 @@
 
 (defn dumb-layout [widget opts]
   (apply dom/div nil
-    (map widget
-      (:widgets opts))))
+    (map widget (:widgets opts))))
 
 (defn- form-group [spec owner cls & content]
   (let [css-class (str cls " " (or (om/get-state owner :group-class)
@@ -26,18 +25,19 @@
         "form-control"))
 
     :layout
-    (fn [owner content]
-      (condp = (:render spec)
+    (spec :layout
+      (fn [owner content]
+        (condp = (:render spec)
 
-        w/checkbox
-        (form-group spec owner nil
-          (dom/div #js {:className "checkbox"}
-            (dom/label nil content
-              (:label spec))))
+          w/checkbox
+          (form-group spec owner nil
+            (dom/div #js {:className "checkbox"}
+              (dom/label nil content
+                (:label spec))))
 
-        (form-group spec owner "form-group"
-          (some->> (:label spec) (dom/label nil))
-          content)))))
+          (form-group spec owner "form-group"
+            (some->> (:label spec) (dom/label nil))
+            content))))))
 
 (defn bootstrap-form-layout [widget opts]
   (apply dom/form nil
