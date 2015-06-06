6acc832f1ed38dd7ed59f2d615139530bcc490ad
Ilshad Khabibullin <astoon.net@gmail.com>
2015-06-06 11:03:07 +0300

    spreadsheet example: stupid formula interpreter

--- examples/examples/spreadsheet.cljs
+++ examples/examples/spreadsheet.cljs
@@ -2,30 +2,78 @@
   (:require [combo.api :as combo]
             [cljs.core.match :refer-macros [match]]
             [om-tools.dom :as dom :include-macros true]
-            [om.core :as om :include-macros true]))
+            [om.core :as om :include-macros true]
+            [clojure.string :as string]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Formula interpreter
+
+(def ops {"/" / "*" * "+" + "-" -})
+
+(defn resolve-var [xy env]
+  (get-in env [:cells xy :value]))
+
+(defn string->token [s env]
+  (or (ops s)
+      (let [n (js/parseInt s)]
+        (if (integer? n) n (resolve-var s env)))))
+
+(defn parse-formula [s env]
+  (let [[_ a b c] (string/split s #" ")]
+    (map #(string->token % env) (list b a c))))
+
+(defn eval-formula [tokens]
+  (apply (first tokens) (rest tokens)))
+
+(defn read-formula [s env]
+  (eval-formula (parse-formula s env)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Look and feel
 
 (defn cell-mode [mode]
-  (fn [xy]
-    [[:cell xy] :class (str mode " cell col-xs-2")]))
+  (fn [xy] [[:cell xy] :class (str mode " cell col-xs-2")]))
 
 (def edit-mode    (cell-mode "edit-mode"))
 (def display-mode (cell-mode "display-mode"))
 (def formula-mode (cell-mode "formula-mode"))
 (def source-mode  (cell-mode "source-mode"))
 
-(defn formula? [v]
-  (string? (first (re-matches #"^=.*" v))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helpers
+
+(defn- formula? [s]
+  (string? (first (re-matches #"^=.*" s))))
+
+(defn- write-formula [s state]
+  (let [formula (get-in state [:cells (:focus state) :formula])]
+    (str formula " " s)))
+
+(defn- cell-value [xy state]
+  (let [cell (get-in state [:cells xy])]
+    (if (:formula cell)
+      (read-formula (:formula cell) state)
+      (:value cell))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The Brain
 
 (defn behavior [message state]
-  (println message "::" state)
   (match message
 
     [[:display xy] :click _]
     (if (= (:mode state) :formula)
-      (let [formula (str (state (:focus state)) xy)]
-        [[(source-mode xy) [[:edit (:focus state)] :value formula]]
-         (assoc state (:focus state) formula)])
-      [[(edit-mode xy) (display-mode (:edit state))]
+      
+      (let [formula (write-formula xy state)]
+        [[(source-mode xy)
+          (display-mode (:source state))
+          [[:edit (:focus state)] :value formula]]
+         (assoc state
+           (:focus state)
+           formula :source xy)])
+      
+      [[(edit-mode xy)
+        (display-mode (:edit state))]
        (assoc state :edit xy)])
     
     [[:edit xy] :focus? true]
@@ -34,19 +82,28 @@
     [[:edit xy] :focus? false]
     (if (= (:mode state) :formula)
       [[] state]
-      [[(display-mode xy) [[:display xy] :value (state (:focus state))]]
+      [[(display-mode xy)
+        [[:display xy] :value (cell-value (:focus state) state)]]
        (assoc state :mode :done)])
 
     [[:edit xy] :value v]
     (if (formula? v)
-      [[(formula-mode xy)] (assoc state :mode :formula xy v)]
-      [[] (assoc state :mode :value xy v)])
+      [[(formula-mode xy)]
+       (-> (assoc state :mode :formula)
+           (assoc-in [:cells xy :formula] v))]
+      [[] (-> (assoc state :mode :value)
+              (assoc-in [:cells xy :value] v))])
 
     [[:edit xy] :key-down 13]
-    [[(display-mode xy) [[:display xy] :value (state (:focus state))]]
+    [[(display-mode xy)
+      (display-mode (:source state))
+      [[:display xy] :value (get-in state [:cells (:focus state) :value])]]
      (assoc state :mode :done)]
     
     :else [[] state]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The Spec
 
 (defn cell [x y]
   (let [xy (str x y)]
@@ -64,12 +121,15 @@
   {:entity [:row y]
    :render combo/div
    :class "row"
-   :units (map #(cell % y) ["A" "B" "C" "D" "E" "F"])})
+   :units (map #(cell % y) ["A" "B" "C" "D" "E"])})
 
 (defn table []
   {:entity :table
    :render combo/div
    :units (map row (range 10))})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public API
 
 (defn spreadsheet [app owner]
   (om/component
